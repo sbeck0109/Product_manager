3e3b742b3cab2f4d3564c13018900049bdfb33b6
Sawyee Beck <sbeck0109@gmail.com>
2020-05-19 03:26:36 -0700

    Product_manager

---
+++ server.js
@@ -0,0 +1,17 @@
+// const port = 8000;
+const express = require("express");
+const cors = require("cors")
+const app = express();
+
+require('./server/config/mongoose.config');
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+require('./server/routes/person.routes')(app);
+
+app.listen(8000, () => {
+  console.log("listening at port 8000")
+})
+
+// app.listen(port, () => console.log(`listening on port: ${port}`));
